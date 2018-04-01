5fcd1773f82b594723ed1cc3f3c077fd62df9796
Nyamdorj Tsereg <mr.tsereg@gmail.com>
2018-04-01 19:24:45 +0200

    fix typos

--- install_dependecies.sh
+++ install_dependecies.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt -y update && sudo apt -y upgrade
-sudo apt -y install git ufw openssh-server libpam-google-authenticator ffmeg thunar kdenlive
-
-#plex
+sudo apt -y install git ufw openssh-server libpam-google-authenticator ffmpeg thunar kdenlive
 
 
